4147329b6362693d59d4f18e1cd9b7f6c200620e
kalyani <kalyani.shinde1112@gmail.com>
2023-03-15 12:43:54 +0530

    home page implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MedicineHomeComponent } from './components/medicine-home/medicine-home.component';
+ 
+const routes: Routes = [
+  
+  {path:'home',component:HomeComponent},
+  {path:'medicine-home',component:MedicineHomeComponent},
+  // {path:'search',component:SearchComponent},
 
-const routes: Routes = [];
+  
+  {path:'**',redirectTo:'/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { CoreModule } from "./core/core.module";
+import { MedicineHomeComponent } from './components/medicine-home/medicine-home.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { config } from 'rxjs';
+import { TopDealsComponent } from './components/top-deals/top-deals.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        MedicineHomeComponent,
+        TopDealsComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CoreModule,
+        CarouselModule,
+        BrowserAnimationsModule
+        
+         
+        
+    ]
 })
 export class AppModule { }
